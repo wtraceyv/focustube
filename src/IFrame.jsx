837ad2e3128fb6d4eaee012315c0d9f7491432ce
walter <wtraceyv@gmail.com>
2022-06-26 16:34:43 -0400

    IFrame can dynamically load different videos

--- src/IFrame.jsx
+++ src/IFrame.jsx
@@ -1,11 +1,67 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
-function IFrame() {
+function vidSrcTemplate(id) {
+  var prefix = "https://www.youtube.com/embed/";
+  var suffix = "?autoplay=0";
+  return prefix + id + suffix;
+}
+
+function test() {
+  console.log("hey there");
+}
+
+var ytplayer;
+var exampleIDs = ['KedW5IUJrLI', 'a1UZRzbiE8Y', 'gGRz95Ry9ok'];
+
+const IFrame = () => {
+  const [curVideoID, setCurVideoID] = useState(0);
+  const [curVideoURL, setCurVideoURL] = useState("https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=0");
+
+  function check() {
+    if (window.YT && window.YT.Player) {
+      console.log("YT is a thing");
+      ytplayer = new window.YT.Player('playerSpace', {
+        height: '390',
+        width: '640',
+        videoId: 'M7lc1UVf-VE',
+      });
+    }
+  }
+
+  function switchVid() {
+    setCurVideoID((curVideoID + 1) % exampleIDs.length);
+    setCurVideoURL(vidSrcTemplate(exampleIDs[curVideoID]));
+    ytplayer.loadVideoByUrl(curVideoURL, 0);
+  }
+
+  useEffect(() => {
+    if (!window.YT) {
+      console.log("Staring IFrame Player API load..");
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      window.onYouTubePlayerAPIReady = check;
+    }
+    else {
+      check();
+    }
+  }, [])
+
   return (
     <div className='iframe-area'>
-      <h1>Video goes here</h1>
+      <div id="playerSpace"></div>
+      <Button
+        onClick={() => switchVid()}
+      >
+        Change Video
+      </Button>
     </div>
   );
-}
+
+};
 
 export default IFrame;
